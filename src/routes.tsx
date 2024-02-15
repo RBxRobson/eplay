e714f176820d96c2d236ef50e6880a68b5f89065
Robson.B <robsonnbarbosa0@gmail.com>
2024-02-15 20:16:19 -0300

    feat - adicionando a rota para produtos

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Categorys from './pages/Categorys'
+import Product from './pages/Product'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categorias" element={<Categorys />} />
+    <Route path="/product/:id" element={<Product />} />
   </Routes>
 )
 
